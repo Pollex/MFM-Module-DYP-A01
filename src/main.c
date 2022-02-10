9fef6d0cc29489147f4b4187258f7c3d6efdcd0d
Tim van Osch <timvosch@pollex.nl>
2022-02-10 13:53:11 +0100

    Implement DS18B20 firmware, send measurement MSB

--- src/main.c
+++ src/main.c
@@ -9,6 +9,9 @@
 #include <os/linked_queue.h>
 #include <mcu/twi.h>
 #include <perif/sen0313.h>
+#include <perif/ds18b20.h>
+#include <drivers/onewire.h>
+#include <mcu/util.h>
 
 #define TWI_CMD_PERFORM 0x10
 #define TWI_CMD_READ 0x11
@@ -29,8 +32,11 @@ sen0313_t sen0313 = {
 int main(void)
 {
   sei(); // Enable interrupts
+  delay_init();
   SENSOR_PWR_PORT.DIRSET = 1 << SENSOR_PWR_PIN;
   SENSOR_PWR_PORT.OUTCLR = 1 << SENSOR_PWR_PIN;
+
+  OW_PORT.DIRCLR = 1 << OW_PIN;
 
   os_init();
   twi_init(0x36, 1);
@@ -43,15 +49,21 @@ int main(void)
 }
 
 volatile uint16_t reading = 0xffff;
+volatile int16_t temperature = 0;
 void measurement(void)
 {
   SENSOR_PWR_PORT.OUTSET = 1 << SENSOR_PWR_PIN;
+
+  // Read SEN0313
   sen0313_init(&sen0313);
   sen0313_read(&sen0313);
   reading = sen0313_read(&sen0313);
+
+  // Read temperature
+  float t = ds18b20_read(0);
+  temperature = (int16_t)(t * 10);
+
   SENSOR_PWR_PORT.OUTCLR = 1 << SENSOR_PWR_PIN;
-  // Alert through SMBAlert
-  // smbus_alert()
 }
 
 void twi_perform(uint8_t *buf, uint8_t length)
@@ -61,9 +73,11 @@ void twi_perform(uint8_t *buf, uint8_t length)
 
 void twi_read(uint8_t *buf, uint8_t length)
 {
-  buf[0] = 2;
-  buf[1] = reading & 0xff;
-  buf[2] = (reading >> 8) & 0xff;
+  buf[0] = 4;
+  buf[1] = (reading >> 8) & 0xff;
+  buf[2] = reading & 0xff;
+  buf[3] = (temperature >> 8) & 0xff;
+  buf[4] = temperature & 0xff;
 }
 
 twi_cmd_t twi_cmds[] = {
